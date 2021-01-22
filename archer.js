79f0763947cbbbcea3eed2cbacc04fa852094794
algorithm0r <algorithm0r@gmail.com>
2021-01-22 01:37:15 -0800

    Added attack animations.

--- archer.js
+++ archer.js
@@ -9,11 +9,18 @@ class Archer {
 
         this.animations = [];
         this.animations.push([]);
-        this.animations[0].push(new Animator(this.spritesheet, 4, 10, 48, 48, 5, 0.2, 26, false, true));
-        this.animations[0].push(new Animator(this.spritesheet, 64, 10, 48, 48, 5, 0.2, 26, false, true));
-        this.animations[0].push(new Animator(this.spritesheet, 128, 10, 48, 48, 5, 0.2, 26, false, true));
-        this.animations[0].push(new Animator(this.spritesheet, 183, 10, 48, 48, 5, 0.2, 26, false, true));
-        this.animations[0].push(new Animator(this.spritesheet, 239, 10, 48, 48, 5, 0.2, 26, false, true));
+        this.animations[0].push(new Animator(this.spritesheet, 4, 10, 48, 48, 5, 0.25, 26, false, true));
+        this.animations[0].push(new Animator(this.spritesheet, 64, 10, 48, 48, 5, 0.25, 26, false, true));
+        this.animations[0].push(new Animator(this.spritesheet, 128, 10, 48, 48, 5, 0.25, 26, false, true));
+        this.animations[0].push(new Animator(this.spritesheet, 183, 10, 48, 48, 5, 0.25, 26, false, true));
+        this.animations[0].push(new Animator(this.spritesheet, 239, 10, 48, 48, 5, 0.25, 26, false, true));
+
+        this.animations.push([]);
+        this.animations[1].push(new Animator(this.spritesheet, 4, 372, 48, 64, 2, 0.5, 10, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 64, 372, 48, 64, 2, 0.5, 10, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 128, 372, 48, 64, 2, 0.5, 10, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 183, 372, 48, 64, 2, 0.5, 10, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 239, 372, 48, 64, 2, 0.5, 10, false, true));
 
         this.state = 0; // 0 walking, 1 attacking, 2 dead
 
@@ -38,5 +45,12 @@ class Archer {
             this.animations[this.state][8 - this.facing].drawFrame(this.game.clockTick, ctx, -(this.x) - 48, this.y, 1);
             ctx.restore();
         }
+        var offset = 64;
+        this.animations[1][0].drawFrame(this.game.clockTick, ctx, this.x + offset, this.y, 1);
+        this.animations[1][1].drawFrame(this.game.clockTick, ctx, this.x + offset + offset, this.y, 1);
+        this.animations[1][2].drawFrame(this.game.clockTick, ctx, this.x + offset * 2 + offset, this.y, 1);
+        this.animations[1][3].drawFrame(this.game.clockTick, ctx, this.x + offset * 3 + offset, this.y, 1);
+        this.animations[1][4].drawFrame(this.game.clockTick, ctx, this.x + offset * 4 + offset, this.y, 1);
+
     };
 };
--- footman.js
+++ footman.js
@@ -16,11 +16,11 @@ class Footman {
         this.animations[0].push(new Animator(this.spritesheet, 308, 9, 48, 53, 5, 0.2, 0, false, true));
 
         this.animations.push([]);
-        this.animations[1].push(new Animator(this.spritesheet, 0, 279, 58, 58, 4, 1, 0, false, true));
-        this.animations[1].push(new Animator(this.spritesheet, 77, 279, 64, 58, 4, 1, 0, false, true));
-        this.animations[1].push(new Animator(this.spritesheet, 150, 279, 64, 58, 4, 1, 0, false, true));
-        this.animations[1].push(new Animator(this.spritesheet, 233, 279, 64, 58, 4, 1, 0, false, true));
-        this.animations[1].push(new Animator(this.spritesheet, 312, 279, 58, 58, 4, 1, 0, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 0, 277, 64, 64, 4, 0.2, 0, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 77, 277, 64, 64, 4, 0.2, 0, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 150, 277, 64, 64, 4, 0.2, 0, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 233, 277, 64, 64, 4, 0.2, 0, false, true));
+        this.animations[1].push(new Animator(this.spritesheet, 312, 277, 64, 64, 4, 0.2, 0, false, true));
 
         this.state = 0; // 0 walking, 1 attacking, 2 dead
 
@@ -45,5 +45,11 @@ class Footman {
             this.animations[this.state][8 - this.facing].drawFrame(this.game.clockTick, ctx, -(this.x) - 48, this.y, 1);
             ctx.restore();
         }
+        var offset = 64;
+        this.animations[1][0].drawFrame(this.game.clockTick, ctx, this.x, this.y + offset, 1);
+        this.animations[1][1].drawFrame(this.game.clockTick, ctx, this.x + offset, this.y + offset, 1);
+        this.animations[1][2].drawFrame(this.game.clockTick, ctx, this.x + offset * 2, this.y + offset, 1);
+        this.animations[1][3].drawFrame(this.game.clockTick, ctx, this.x + offset * 3, this.y + offset, 1);
+        this.animations[1][4].drawFrame(this.game.clockTick, ctx, this.x + offset * 4, this.y + offset, 1);
     };
 };
